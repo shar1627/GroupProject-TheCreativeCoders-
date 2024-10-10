5681e8b280ddad119be70b4311cb6e9528c8ddf8
Rishabdeep <sin15506@sheridancollege.ca>
2024-10-10 17:36:41 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -9,19 +9,26 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author dancye
+ * @author Rishabdeep Singh 10-09-2024
  */
-public abstract class Card {
-    //default modifier for child classes
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+import java.util.List;
+
+public class Card {
+    private List<String> symbols; 
+
+    public Card(List<String> symbols) {
+        this.symbols = symbols;
+    }
+
     @Override
-    public abstract String toString();
+    public String toString() {
+        return "Card{" +
+                "symbols=" + symbols +
+                '}';
+    }
 
+    public List<String> getSymbols() {
+        return symbols;
+    }
 }
-
-
