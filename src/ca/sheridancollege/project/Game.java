7b02d8ac75179a5f3ef818730c82af5fe41817ac
shar1627 <shar1627@sheridancollege.ca>
2024-10-10 17:32:09 -0400

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -2,58 +2,101 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+ * @author Shivansh Sharma 10-09-2024
  */
 package ca.sheridancollege.project;
 
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-/**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Game {
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+public class Game {
+    private final String name; 
+    private ArrayList<Player> players;
+    private GroupOfCards deck; 
+    private Scanner scanner;
 
     public Game(String name) {
         this.name = name;
-        players = new ArrayList();
+        this.players = new ArrayList<>();
+        this.deck = new GroupOfCards(55); 
+        this.scanner = new Scanner(System.in);
+        initializeDeck();
     }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
+    private void initializeDeck() {
+        deck.addCard(new Card(List.of("A", "B", "C")));
+        deck.addCard(new Card(List.of("A", "D", "E")));
+        deck.addCard(new Card(List.of("C", "E", "F"))); 
+        deck.addCard(new Card(List.of("D", "E", "C")));
+        deck.addCard(new Card(List.of("D", "A", "G")));
+        deck.shuffle(); 
     }
 
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
+    public void addPlayer(Player player) {
+        players.add(player);
     }
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
+    public void dealCards(int cardsPerPlayer) {
+        for (Player player : players) {
+            for (int i = 0; i < cardsPerPlayer; i++) {
+                if (!deck.getCards().isEmpty()) {
+                    player.addCard(deck.getCards().remove(0)); 
+            }
+        }
     }
 
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
+    public void play() {
+        System.out.println("Starting the game: " + name);
+        for (Player player : players) {
+            System.out.println(player.getName() + ", it's your turn! Choose a card from your hand:");
+            for (int i = 0; i < player.getHand().size(); i++) {
+                System.out.println(i + 1 + ": " + player.getHand().get(i));
+            }
+            System.out.print("Enter the number of the card you want to play: ");
+            int choice = scanner.nextInt();
+            // Perform action based on player's choice
+            if (choice > 0 && choice <= player.getHand().size()) {
+                Card selectedCard = player.getHand().get(choice - 1);
+                System.out.println(player.getName() + " has chosen: " + selectedCard);
+                player.incrementCardsPlayed(); 
+            } else {
+                System.out.println("Invalid choice! Please choose again.");
+            }
+        }
+    }
 
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
+    public void declareWinner() {
+        // Determine the winner based on cards played
+        Player winner = players.get(0);
+        for (Player player : players) {
+            if (player.getCardsPlayed() > winner.getCardsPlayed()) {
+                winner = player; 
+            }
+        }
+        System.out.println("Winner is: " + winner.getName() + " with " + winner.getCardsPlayed() + " cards played!");
+    }
 
-}//end class
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Game game = new Game("Spot It!");
+
+        System.out.print("Enter the number of players: ");
+        int numPlayers = scanner.nextInt();
+        scanner.nextLine(); 
+
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.print("Enter name for Player " + (i + 1) + ": ");
+            String playerName = scanner.nextLine();
+            game.addPlayer(new Player(playerName));
+        }
+
+        game.dealCards(3);
+
+        game.play();
+        game.declareWinner();
+    }
+}
 
